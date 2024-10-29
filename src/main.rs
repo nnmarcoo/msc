43dd9f6afd0e941a420e5edfb5ff0d380956955c
Marco <marcosteam7@gmail.com>
2024-10-29 10:04:50 -0400

    boiler

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod msc;
+
+use msc::Msc;
+use eframe::{egui::ViewportBuilder, run_native, Error, NativeOptions, Result};
+
+fn main() -> Result<(), Error> {
+    let native_options = NativeOptions {
+        viewport: ViewportBuilder::default()
+        .with_title("msc"),
+        ..Default::default()
+    };
+    run_native(
+        "msc",
+        native_options,
+        Box::new(|cc| Ok(Box::new(Msc::new(cc)))),
+    )
+}
---
+++ src/msc.rs
@@ -0,0 +1,25 @@
+use eframe::{
+    egui::{CentralPanel, Context},
+    App, CreationContext, Frame,
+};
+
+pub struct Msc {
+}
+
+impl Default for Msc {
+    fn default() -> Self {
+        Self {  }
+    }
+}
+
+impl Msc {
+    pub fn new(_cc: &CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl App for Msc {
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+        
+    }
+}
