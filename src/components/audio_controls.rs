886c8e83526440b6f58b7dbd740aa8f47a795416
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2024-11-07 10:16:22 -0500

    default value

--- src/components/audio_controls.rs
+++ src/components/audio_controls.rs
@@ -1,28 +1,44 @@
+use std::io::Cursor;
+
 use crate::{util::get_volume_color, widgets::color_slider::color_slider};
 use eframe::egui::{include_image, Color32, Context, ImageButton, TopBottomPanel};
+use kira::{manager::{AudioManager, AudioManagerSettings, DefaultBackend}, sound::{streaming::{StreamingSoundData, StreamingSoundHandle}, FromFileError, PlaybackState}};
+use kira::tween::Tween;
+use kira::Volume;
 
 pub struct AudioControls {
     is_playing: bool,
     volume: f32,
     handle_pos: f32,
     is_timeline_dragged: bool,
+    manager: AudioManager,
+    sound: StreamingSoundHandle<FromFileError>,
 }
 
 impl AudioControls {
     pub fn new() -> Self {
+        let mut manager = AudioManager::<DefaultBackend>::new(AudioManagerSettings::default()).unwrap();
+
+        let silent_audio_data: Vec<u8> = vec![0; 44100 * 2 * 2]; // 1 second of silence for 16-bit stereo at 44.1 kHz
+        let cursor = Cursor::new(silent_audio_data);
+        let stream = StreamingSoundData::from_cursor(cursor).unwrap();
+        let sound = manager.play(stream).unwrap();
+
         AudioControls {
             is_playing: false,
             volume: 1.,
             handle_pos: 0.,
             is_timeline_dragged: false,
+            manager,
+            sound,
         }
     }
 
-    pub fn show(&mut self, ctx: &Context) {
+    pub fn show(&mut self, ctx: &Context) -> Result<(), &'static str> {
         if self.is_playing {
             ctx.request_repaint_after(std::time::Duration::from_millis(100));
             if !self.is_timeline_dragged {
-                //self.handle_pos = self.sink.get_pos().as_secs_f32();
+                //self.handle_pos = self.get_sound()?.position() as f32;
             }
         }
 
@@ -35,17 +51,7 @@ impl AudioControls {
                     include_image!("../../assets/icons/play.png")
                 };
 
-                ui.horizontal(|ui| {
-                    if ui
-                        .add_sized(
-                            [30., 30.],
-                            ImageButton::new(include_image!("../../assets/icons/x.png"))
-                                .rounding(100.),
-                        )
-                        .clicked()
-                    {
-                        //self.toggle_playback();
-                    }
+                ui.horizontal(|ui|  {
 
                     if ui
                         .add_sized([30., 30.], ImageButton::new(icon).rounding(100.))
@@ -53,59 +59,9 @@ impl AudioControls {
                     {
                         //self.toggle_playback();
                     }
-
-                    if ui
-                        .add_sized(
-                            [30., 30.],
-                            ImageButton::new(include_image!("../../assets/icons/x.png"))
-                                .rounding(100.),
-                        )
-                        .clicked()
-                    {
-                        //self.toggle_playback();
-                    }
                 });
 
-                ui.horizontal(|ui| {
-                    //ui.label(seconds_to_string(self.handle_pos));
-
-                    let timeline_res = ui.add(color_slider(
-                        &mut self.handle_pos,
-                        0.0..=1.0, //
-                        200.,
-                        8.,
-                        6.,
-                        Color32::from_rgb(0, 92, 128),
-                    ));
-
-                    if timeline_res.drag_stopped() || timeline_res.clicked() {
-                        //self.sink.try_seek(Duration::new(self.handle_pos as u64, 0)).unwrap();
-                    }
-
-                    if timeline_res.is_pointer_button_down_on() || timeline_res.dragged() {
-                        self.is_timeline_dragged = true;
-                    } else {
-                        self.is_timeline_dragged = false;
-                    }
-
-                    //ui.label(duration_to_string(self.track_length));
-
-                    let volume_color = get_volume_color(1.);
-
-                    if ui
-                        .add(color_slider(
-                            &mut self.volume,
-                            0.0..=2.0,
-                            100.,
-                            8.,
-                            6.,
-                            volume_color,
-                        ))
-                        .changed()
-                    {
-                        //self.sink.set_volume(self.volume);
-                    }
-                });
             });
+            Ok(())
     }
 }
