923388c19e71959a839c01e26e678222e167bbc9
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2024-11-08 23:22:25 -0500

    fmt

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,7 @@
 use eframe::egui::{
     Color32, ColorImage, Context, CursorIcon, Pos2, ResizeDirection, ViewportCommand,
 };
-use image::load_from_memory;
+use image::{imageops::FilterType, load_from_memory};
 
 use lofty::{
     file::{AudioFile, TaggedFileExt},
@@ -109,6 +109,7 @@ pub fn get_volume_color(value: f32) -> Color32 {
     }
 }
 
+// change and make separate struct for tracks
 pub fn get_audio_metadata(file_path: &str) -> Result<AudioMetadata, Box<dyn std::error::Error>> {
     let tagged_file = Probe::open(file_path)?.read()?;
     let properties = tagged_file.properties();
@@ -123,25 +124,25 @@ pub fn get_audio_metadata(file_path: &str) -> Result<AudioMetadata, Box<dyn std:
 
     let duration = properties.duration().as_secs_f32();
 
-    let mut image = ColorImage::example();
+    let mut image = ColorImage::example(); // change
 
     if let Some(picture) = tagged_file
         .primary_tag()
         .and_then(|tag| tag.pictures().first())
     {
         let image_data = picture.data();
-        let img = load_from_memory(image_data).ok().unwrap();
+        let img =
+            load_from_memory(image_data)
+                .ok()
+                .unwrap()
+                .resize_exact(50, 50, FilterType::Lanczos3);
+        let img = img.resize_exact(50, 50, FilterType::Lanczos3);
 
-        let img = img.to_rgba8();
-        let size = [img.width() as usize, img.height() as usize];
-        let pixels = img.into_raw();
+        let rgba_img = img.to_rgba8();
+        let size = [rgba_img.width() as usize, rgba_img.height() as usize];
+        let pixels = rgba_img.into_raw();
 
-        image = ColorImage::from_rgba_unmultiplied(size, &pixels) /* Some(ctx.load_texture(
-                                                                      "loaded_image",
-                                                                      ColorImage::from_rgba_unmultiplied(size, &pixels),
-                                                                      TextureOptions::default(),
-                                                                  ));
-                                                                  */
+        image = ColorImage::from_rgba_unmultiplied(size, &pixels)
     }
 
     Ok(AudioMetadata {
