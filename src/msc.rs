0bae7c71389dc2ac47fb303aded2c269f66f7ac6
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2024-12-05 16:03:48 -0500

    Update msc.rs

--- src/msc.rs
+++ src/msc.rs
@@ -80,7 +80,7 @@ impl App for Msc {
                     .inner_margin(Margin::ZERO)
                     .fill(ctx.style().visuals.panel_fill),
             )
-            .show(ctx, |ui| {
+            .show(ctx, |_ui| {
                 handle_resize(self, ctx);
 
                 self.title_bar.show(ctx, &mut self.state);
