29fb0a261789f222be56c583852416ce28f0f83f
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2024-12-11 22:16:35 -0500

    list view playlist

--- src/components/main_area.rs
+++ src/components/main_area.rs
@@ -104,6 +104,21 @@ impl MainArea {
             });
         });
 
+        if self.show_window {
+            Window::new("Edit playlist")
+                .open(&mut self.show_window)
+                .collapsible(false)
+                .anchor(Align2::CENTER_CENTER, vec2(0., 0.))
+                .resizable(false)
+                .default_pos(pos2(ui.available_width() / 2., ui.available_height() / 2.))
+                .show(ctx, |ui| {
+                    ui.add(TextEdit::singleline(&mut playlist.name).hint_text("Name"));
+                    ui.add(TextEdit::singleline(&mut playlist.desc).hint_text("Description"));
+                });
+        } else if playlist.name.is_empty() {
+            playlist.name = String::from("My Playlist");
+        }
+
         if playlist.tracks.is_empty() {
             ui.vertical(|ui| {
                 ui.add_space(ui.available_height() / 2. - 20.);
@@ -124,22 +139,87 @@ impl MainArea {
                     }
                 });
             });
+            return;
         }
 
-        if self.show_window {
-            Window::new("Edit playlist")
-                .open(&mut self.show_window)
-                .collapsible(false)
-                .anchor(Align2::CENTER_CENTER, vec2(0., 0.))
-                .resizable(false)
-                .default_pos(pos2(ui.available_width() / 2., ui.available_height() / 2.))
-                .show(ctx, |ui| {
-                    ui.add(TextEdit::singleline(&mut playlist.name).hint_text("Name"));
-                    ui.add(TextEdit::singleline(&mut playlist.desc).hint_text("Description"));
+        ui.add_space(10.);
+
+        let track_num_width = ui.fonts(|fonts| {
+            let font_id = ui.style().text_styles[&TextStyle::Body].clone();
+
+            fonts
+                .layout_no_wrap(
+                    format!("{}.", playlist.tracks.len()),
+                    font_id.clone(),
+                    Color32::TRANSPARENT,
+                )
+                .size()
+                .x
+        });
+
+        let duration_width = 112. - track_num_width;
+
+        let available_width =
+            ((ui.available_width() - track_num_width - duration_width) / 3.).max(0.);
+
+        TableBuilder::new(ui)
+            .scroll_bar_visibility(ScrollBarVisibility::AlwaysHidden)
+            .column(Column::exact(track_num_width))
+            .column(Column::exact(available_width))
+            .column(Column::exact(available_width))
+            .column(Column::exact(available_width))
+            .column(Column::exact(duration_width))
+            .sense(Sense::click())
+            .header(20., |mut header| {
+                for text in HEADERS {
+                    header.col(|ui| {
+                        ui.strong(text);
+                    });
+                }
+            })
+            .body(|body| {
+                body.rows(16., playlist.tracks.len(), |mut row| {
+                    let index = row.index();
+                    let track = &playlist.tracks[index];
+
+                    row.set_selected(self.selection.contains(&index));
+
+                    row.col(|ui| {
+                        ui.label(format!("{}.", index));
+                    });
+                    row.col(|ui| {
+                        ui.add(Label::new(&track.title).wrap_mode(TextWrapMode::Truncate));
+                    });
+                    row.col(|ui| {
+                        ui.add(Label::new(&track.artist).wrap_mode(TextWrapMode::Truncate));
+                    });
+                    row.col(|ui| {
+                        ui.add(Label::new(&track.album).wrap_mode(TextWrapMode::Truncate));
+                    });
+                    row.col(|ui| {
+                        ui.label(format_seconds(track.duration));
+                    });
+
+                    let response = row.response();
+
+                    response.context_menu(|ui| {
+                        ui.menu_button("Remove from playlist", |ui| {
+                            if ui.button("Confirm").clicked() {
+                                playlist.tracks.remove(index);
+                                self.selection.remove(&index);
+                                ui.close_menu();
+                            }
+                        });
+
+                        if ui.button("Play").clicked() {
+                            ui.close_menu();
+                            // TODO: Play selected track
+                        }
+                    });
+
+                    self.toggle_row_selection(index, &response);
                 });
-        } else if playlist.name.is_empty() {
-            playlist.name = String::from("My Playlist");
-        }
+            });
     }
 
     fn show_search(&mut self, ui: &mut Ui, _state: &mut State) {
