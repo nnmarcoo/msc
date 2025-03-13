e1f9f5cb5c1870e74e75312a6c2bf909b29fb723
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2025-03-12 23:26:41 -0400

    title bar start

--- src/components/title_bar.rs
+++ src/components/title_bar.rs
@@ -1,14 +1,26 @@
-use egui::{Context, Frame, Margin, TopBottomPanel};
+use eframe::egui::{
+    include_image,
+    menu::{self},
+    vec2, Align, Color32, Context, Frame, ImageButton, Layout, Margin, PointerButton, Sense,
+    TextEdit, TopBottomPanel, ViewportCommand,
+};
+use serde::{Deserialize, Serialize};
+
+use crate::structs::WindowState;
+
+#[derive(Serialize, Deserialize, Default)]
 pub struct TitleBar {
-    pub is_dragging: bool,
-    pub is_maximized: bool,
+    pub window_state: WindowState,
+    pub query: String,
+    bar_width: f32,
 }
 
 impl TitleBar {
     pub fn new() -> Self {
         TitleBar {
-            is_dragging: false,
-            is_maximized: false,
+            window_state: WindowState::default(),
+            query: String::new(),
+            bar_width: 30.,
         }
     }
 
@@ -16,7 +28,107 @@ impl TitleBar {
         TopBottomPanel::top("title_bar")
             .frame(Frame::default().inner_margin(Margin::ZERO))
             .show(ctx, |ui| {
-                ui.label("TODO");
+                self.window_state.is_maximized =
+                    ui.input(|i| i.viewport().maximized.unwrap_or(false));
+
+                let res = ui.interact(ui.max_rect(), ui.id(), Sense::click_and_drag());
+
+                if res.drag_started_by(PointerButton::Primary) {
+                    ctx.send_viewport_cmd(ViewportCommand::StartDrag);
+                    self.window_state.is_dragging = true;
+                }
+
+                if res.drag_stopped() {
+                    self.window_state.is_dragging = false;
+                }
+
+                if res.double_clicked_by(PointerButton::Primary) {
+                    ctx.send_viewport_cmd(ViewportCommand::Maximized(
+                        !self.window_state.is_maximized,
+                    ));
+                }
+
+                menu::bar(ui, |ui| {
+                    // Remove spacing between widgets
+                    ui.spacing_mut().item_spacing = vec2(0.0, 0.0);
+
+                    ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
+                        ui.scope(|ui| {
+                            ui.style_mut().visuals.widgets.hovered.weak_bg_fill =
+                                Color32::from_rgb(232, 17, 35);
+                            if ui
+                                .add_sized(
+                                    [self.bar_width, self.bar_width],
+                                    ImageButton::new(include_image!("../../assets/icon-256.png")),
+                                )
+                                .clicked()
+                            {
+                                ctx.send_viewport_cmd(ViewportCommand::Close);
+                            }
+                        });
+
+                        if self.window_state.is_maximized {
+                            if ui
+                                .add_sized(
+                                    [self.bar_width, self.bar_width],
+                                    ImageButton::new(include_image!("../../assets/icon-256.png")),
+                                )
+                                .clicked()
+                            {
+                                ctx.send_viewport_cmd(ViewportCommand::Maximized(false));
+                            }
+                        } else {
+                            if ui
+                                .add_sized(
+                                    [self.bar_width, self.bar_width],
+                                    ImageButton::new(include_image!("../../assets/icon-256.png")),
+                                )
+                                .clicked()
+                            {
+                                ctx.send_viewport_cmd(ViewportCommand::Maximized(true));
+                            }
+                        }
+
+                        if ui
+                            .add_sized(
+                                [self.bar_width, self.bar_width],
+                                ImageButton::new(include_image!("../../assets/icon-256.png")),
+                            )
+                            .clicked()
+                        {
+                            ctx.send_viewport_cmd(ViewportCommand::Minimized(true));
+                        }
+
+                        if ui
+                            .add(
+                                TextEdit::singleline(&mut self.query)
+                                    .hint_text("🔍 Search a song")
+                                    .desired_width(150.),
+                            )
+                            .has_focus()
+                        {
+                            //state.view = View::Library;
+                        }
+
+                        ui.add_space(ui.available_width() - 47.);
+
+                        ui.allocate_ui(vec2(28., 28.), |ui| {
+                            ui.menu_image_button(
+                                include_image!("../../assets/icon-256.png"),
+                                |ui| {
+                                    if ui.button("About").clicked() {}
+                                    if ui.button("Help").clicked() {}
+                                    if ui.button("Update").clicked() {}
+                                    ui.separator();
+                                    if ui.button("Settings").clicked() {
+                                        //state.view = View::Settings;
+                                        ui.close_menu();
+                                    }
+                                },
+                            );
+                        });
+                    });
+                });
             });
     }
 }
