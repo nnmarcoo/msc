12f3fa06a9e887e0fd4a53e192a08e07168f8a70
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2025-03-12 23:26:01 -0400

    remove decorations

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main() -> eframe::Result {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
             .with_min_inner_size([300.0, 220.0])
+            .with_decorations(false)
             .with_icon(
                 eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
                     .expect("Failed to load icon"),
