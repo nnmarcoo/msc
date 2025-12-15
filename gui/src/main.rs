84a69e3c76cd4d62f0c0e9ec5ceef23e290aaa48
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2025-12-15 15:41:41 -0500

    app title

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -8,7 +8,7 @@ mod widgets;
 use app::App;
 
 pub fn main() -> iced::Result {
-    iced::application("MSC - Music Player", App::update, App::view)
+    iced::application("msc", App::update, App::view)
         .subscription(App::subscription)
         .theme(|_| Theme::CatppuccinFrappe)
         .run()
