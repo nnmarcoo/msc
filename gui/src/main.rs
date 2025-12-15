7055d2196edc272a4966983d6da8e16198e4cdf9
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2025-12-15 15:09:03 -0500

    Update main.rs

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -10,6 +10,6 @@ use app::App;
 pub fn main() -> iced::Result {
     iced::application("MSC - Music Player", App::update, App::view)
         .subscription(App::subscription)
-        .theme(|_| Theme::Nord)
+        .theme(|_| Theme::CatppuccinFrappe)
         .run()
 }
