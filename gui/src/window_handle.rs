2098b5690e650d97399ff18cfc7eb4287d866529
nnmarcoo <79803313+nnmarcoo@users.noreply.github.com>
2025-12-21 20:08:14 -0500

    windows fix

--- gui/src/window_handle.rs
+++ gui/src/window_handle.rs
@@ -1,8 +1,18 @@
 #[cfg(target_os = "windows")]
 pub fn get_hwnd() -> Option<*mut std::ffi::c_void> {
-    use windows::Win32::UI::WindowsAndMessaging::GetForegroundWindow;
+    use windows::Win32::UI::WindowsAndMessaging::{FindWindowW, GetForegroundWindow};
+    use windows::core::PCWSTR;
 
     unsafe {
+        let window_title: Vec<u16> = "msc\0".encode_utf16().collect();
+        let hwnd = FindWindowW(PCWSTR::null(), PCWSTR::from_raw(window_title.as_ptr()));
+
+        if let Ok(hwnd) = hwnd {
+            if !hwnd.0.is_null() {
+                return Some(hwnd.0 as *mut std::ffi::c_void);
+            }
+        }
+
         let hwnd = GetForegroundWindow();
         if !hwnd.0.is_null() {
             Some(hwnd.0 as *mut std::ffi::c_void)
